9b40f862c89a2017445bdca36db14649757fc081
Patrick Maltagliati <patmalt@gmail.com>
2020-11-12 11:13:47 -0600

    Initial Commit

---
+++ Wave/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wave
+//
+//  Created by Patrick Maltagliati on 11/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wave/WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveApp.swift
+//  Wave
+//
+//  Created by Patrick Maltagliati on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
